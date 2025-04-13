ddf2af88fe89522a9fb38af513f980367ba72cd1
fayod7 <fayodnv@gmail.com>
2025-04-13 17:29:02 +0500

    hw

---
+++ app.js
@@ -0,0 +1,148 @@
+{
+    // M-1
+    var a = prompt("a ni kiriting")
+    var p = 4 * a
+    console.log("Perimeter: " + p);
+}
+
+{
+    // M -2
+    // var a = prompt("a sonini kiriting")
+    // var s = a ** 2
+    // console.log("Yuza: " + s);
+}
+
+{
+    // M-3
+    // var a = prompt("a ni kiriting")
+    // var b = prompt("b ni kiriting")
+    // var p = (Number(a) + Number(b)) * 2
+    // var s = a * b
+    // console.log("Yuza: " + s);
+    // console.log("Perimeter: " + p);
+}
+
+{
+    // M-4
+    // var d = prompt("d ni kiriting")
+    // var p = Math.PI
+    // var l = d * p
+    // console.log("Aylana uzunligi: " + l);
+}
+
+{
+    // M-5
+    // var a = prompt("a ni kiriting")
+    // var v = a ** 3
+    // var s = 6 * a ** 2
+    // console.log("Hajm: " + v);
+    // console.log("Yuza: " + s);
+}
+
+{
+    // M-6
+    // var a = prompt("a ni kiriting")
+    // var b = prompt("b ni kiriting")
+    // var c = prompt("c ni kiriting")
+    // var v = a * b * c
+    // var s = 2 * (a * b + b * c + a * c)
+    // console.log("Hajm: " + v);
+    // console.log("To'la sirti: " + s);
+}
+
+{
+    // M-7
+    // var r = prompt("R ni kiriting")
+    // var p = Math.PI
+    // var l = 2 * r * p
+    // var s = p * r ** 2
+    // console.log("Aylana uzunligi: " + l);
+    // console.log("Yuza: " + s);
+}
+
+{
+    // M-8
+    // var a = prompt("a ni kiriting");
+    // var b = prompt("b ni kiriting");
+    // var average = (Number(a) + Number(b))/2;
+    // console.log("O'rta arifmetik: " + average);
+}
+
+{
+    // M-9
+    // var a = prompt("a ni kiriting");
+    // var b = prompt("b ni kiriting");
+    // var gAverage = Math.sqrt((Number(a) * Number(b))) 
+    // console.log("O'rta geometrik: " + gAverage);
+}
+
+{
+    // M-10
+    // var a = prompt("a ni kiriting");
+    // var b = prompt("b ni kiriting");
+    // var yigindi = Number(a) + Number(b)
+    // var kopaytma = a * b
+    // var squareFirst = a ** 2
+    // var squareSecond = b ** 2
+    // console.log("Yig'indi: " + yigindi);
+    // console.log("Ko'paytma: " + kopaytma);
+    // console.log("A kvadrat: " + squareFirst);
+    // console.log("B kvadrat: " + squareSecond);
+}
+
+{
+    // M-11
+    // var a = prompt("a ni kiriting");
+    // var b = prompt("b ni kiriting");
+    // var yigindi = Number(a) + Number(b)
+    // var kopaytma = a * b
+    // var absFirst = Math.abs(a)
+    // var absSecond = Math.abs(b)
+    // console.log("Yig'indi: " + yigindi);
+    // console.log("Ko'paytma: " + kopaytma);
+    // console.log("A modulda: " + absFirst);
+    // console.log("B modulda: " + absSecond);
+}
+
+{
+    // M-12 
+    // var a = prompt("a ni kiriting");
+    // var b = prompt("b ni kiriting");
+    // var c =  Math.sqrt(a ** 2 + b ** 2)
+    // var p = Number(a) + Number(b) + Number(c)
+    // console.log("Gipotenuza: " + c);
+    // console.log("Perimeter: " + p);  
+}
+
+{
+    // M -13
+    // var radiusFirst = prompt("R1 ni kiriting")
+    // var radiusSecond = prompt("R2 ni kiriting")
+    // var p = Math.PI
+    // var areaFirst = radiusFirst * p
+    // var areaSecond = radiusSecond * p
+    // var areaDifference = p * (Number(radiusFirst) - Number(radiusSecond))
+    // console.log("Yuza 1: " + areaFirst);
+    // console.log("Yuza 2: " + areaSecond);
+    // console.log("Yuzalar farqi: " + areaDifference);
+}
+
+{
+    // M-14
+    // var l = prompt("L ni kiriting")
+    // var p = Math.PI
+    // var r = l / 2 * p
+    // var s = p * r ** 2
+    // console.log("Radius: " + r);
+    // console.log("Yuza: " + s);
+}
+
+{
+    // M-15
+    // var s = prompt("S ni kiriting")
+    // var p = Math.PI
+    // var r = Math.sqrt(s / 3,14)
+    // var l = 2 * p * r
+    // console.log("Radius: " + r);
+    // console.log("Aylana uzunligi: " + l);
+}
